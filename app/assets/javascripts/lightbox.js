6b7b35abd18ac10c43a204428434d779ae3128ed
Chris Russell-Walker <c.russell.walker@gmail.com>
2012-11-05 17:38:04 -0500

    lightbox working and favicon - still need to get pdf working

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.gif',
+    fileLoadingImage:        '/assets/loader.gif',     
+    fileBottomNavCloseImage: '/assets/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
@@ -106,7 +106,7 @@ Lightbox.prototype = {
         //              </div>
         //              <div id="loading">
         //                  <a href="#" id="loadingLink">
-        //                      <img src="images/loading.gif">
+        //                      <img src="/assets/loader.gif">
         //                  </a>
         //              </div>
         //          </div>
@@ -119,7 +119,7 @@ Lightbox.prototype = {
         //              </div>
         //              <div id="bottomNav">
         //                  <a href="#" id="bottomNavClose">
-        //                      <img src="images/close.gif">
+        //                      <img src="/assets/close.gif">
         //                  </a>
         //              </div>
         //          </div>
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,13 @@
 class IndexController < ApplicationController
   def index
   end
+  def resume
+  	respond_to do |format|
+  		format.html
+      format.pdf { render :file => "app/assets/resources/resume.pdf" }
+  		#format.pdf {render :file => 'app\assets\resources\resume.pdf', 
+  		#									 :disposition => 'inline',
+      #									 :content_type => 'application/pdf'}
+  	end
+  end
 end
