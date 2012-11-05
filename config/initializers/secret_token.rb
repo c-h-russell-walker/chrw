2604fc99e4859788b2b4aac5d71c51e06109dfc9
Chris Russell-Walker <c.russell.walker@gmail.com>
2012-11-05 15:02:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChrwRails::Application.config.secret_token = 'dfdd9af34eab75038c1434189536f2c8594e22616b0c0d37b7998d91c82b20327bbec7cb4764ea9c991a8998fe26cff3179f5384b3e776b550160d809fc3e87d'
